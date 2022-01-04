8d92ed7112c22968f2595864700c7393bbbeda92
Szymonnowicki1 <92035422+Szymonnowicki1@users.noreply.github.com>
2022-01-04 18:36:10 +0100

    Add files via upload

---
+++ zadanie2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+string imie = "Szymon";
+double waga = 74;
+int wzrost = 185;
+
+int main()
+{
+    cout << "Imie : " << imie << endl;
+    cout << "\n";
+    cout << "Waga : " << waga << "kg" << endl;
+    cout << "\n";
+    cout << "Wzrost : " << wzrost << "cm" << endl;
+
+}
